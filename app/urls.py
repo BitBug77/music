71d763ddd2058087b9b8d2a2e18a127bb7447443
BitBug77 <kcmitakshya10@gmail.com>
2025-02-26 16:13:12 +0545

    updated urls

--- app/urls.py
+++ app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('spotify-callback/', views.spotify_callback, name='spotify_callback'),
     path('home/', views.home, name='home'),
     path('popularity/', views.get_songs_by_popularity, name='songs_by_popularity'),
+    path('recommended-songs/', views.recommended_songs, name='recommended_songs'),
 ]
